14253344b20bf42cd204032b701486eb5d2771a8
Phanxv <phankawee2003@gmail.com>
2024-03-30 15:40:01 +0700

    Connect to mongoDB using docker

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require('dotenv').config();
+
 const app = express();
 
-app.set('view engine', 'ejs');
+require('./initDB.js')();
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(3000, () => {
-    console.log('Listening at port 3000');
+  console.log("Listening at port 3000");
 });
