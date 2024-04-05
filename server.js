c3fb3304f3f4a517b77b97fd552da9d14033d0c5
Phanxv <phankawee2003@gmail.com>
2024-04-05 14:46:31 +0700

    Handling error and create API endpoint for creating user

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const Post = require("./models/Post.js");
+const User = require('./models/User.js');
 const fs = require("fs").promises;
 
 const app = express();
@@ -11,19 +13,55 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./initDB.js")();
+require('./initDB.js')();
 
 app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const posts = await Post.find().lean().exec();
-  res.render("home", { posts });
+  res.render('home', { posts });
 });
 
-app.get("/upload", (req, res) => {
-  res.render("upload");
+app.get('/upload', (req, res) => {
+  res.render('upload');
+});
+
+app.get('/register', (req, res) => {
+  let err_code = 200;
+  res.render('register', { err_code });
+});
+
+app.get('/register/:err', (req, res) => {
+  let err_code = req.params.err;
+  console.log(err_code)
+  res.render('register', { err_code });
+});
+
+app.post("/createUser", async (req, res) => {
+  console.log(req.body);
+  if (req.body.password !== req.body.re_password) {
+    return res.redirect("/register/609");
+  }
+  try {
+    const userData = {
+      username: req.body.username,
+    };
+    userData.password = await bcrypt.hash(req.body.password, 10);
+    console.log(userData);
+    await User.create(userData)
+      .then(() => {
+        console.log("User Created");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    res.send("OK");
+  }
 });
 
 const attachmentStorage = multer.diskStorage({
@@ -35,7 +73,7 @@ const attachmentStorage = multer.diskStorage({
     cb(
       null,
       path.parse(file.originalname).name +
-        "-" +
+        "_" +
         Date.now() +
         path.extname(file.originalname)
     );
@@ -53,50 +91,62 @@ async function deleteFile(filePath) {
   }
 }
 
-app.post("/wipeDatabase", async (req, res) => {
+app.post('/wipeDatabase', async (req, res) => {
   const posts = await Post.find();
   posts.forEach((element) => {
     deleteFile(element.path);
   });
   Post.deleteMany()
-    .then(function () {
-      // Success
+    .then( () => {
       console.log("Database deleted");
     })
-    .catch(function (error) {
-      // Failure
-      console.log(error);
+    .catch( (e) => {
+      console.log(e);
     });
-  res.send("Database wiped")
+  res.send("Database wiped");
 });
 
-app.post("/createPost", upload.single("attachment"), async (req, res) => {
+app.post('/createPost', upload.single("attachment"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
+  try {
+    const postData = {
+      postName: req.body.postName,
+      postDesp: req.body.postDesp,
+      path: req.file.path,
+      originalName: req.file.originalname,
+    };
 
-  const postData = {
-    postName: req.body.postName,
-    postDesp: req.body.postDesp,
-    path: req.file.path,
-    originalName: req.file.originalname,
-  };
+    await Post.create(postData)
+      .then(() => {
+        console.log("Post Created");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
 
-  const post = await Post.create(postData);
-
-  console.log(post);
-
-  res.send("posted");
+  } catch (e) {
+    console.log(e);
+    return res.redirect("/upload");
+  } finally {
+    return res.redirect("/");
+  }
 });
 
-app.get("/fetchPosts", async (req, res) => {
+app.get('/fetchPosts', async (req, res) => {
   const post = await Post.find().lean().exec();
   res.send(post);
 });
 
 app.get('/storage/:fileId', (req, res) => {
   console.log("requested file : " + req.params.fileId);
-  res.sendFile(path.join(__dirname, "./storage/" + req.params.fileId));
-})
+  try {
+    res.sendFile(path.join(__dirname, './storage/' + req.params.fileId));
+    console.log("file served");
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Listening at port 3000");
