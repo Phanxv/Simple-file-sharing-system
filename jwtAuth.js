a79695c232356800819414a3ed65f19636793315
Phanxv <phankawee2003@gmail.com>
2024-04-08 14:32:48 +0700

    update error handling for jwt authentication

--- jwtAuth.js
+++ jwtAuth.js
@@ -3,12 +3,11 @@ const jwt = require('jsonwebtoken')
 exports.jwtAuth = (req, res, next) => {
     try {
         const token = req.cookies.token;
-        console.log(token)
         jwt.verify(token, process.env.PRIVATE_KEY);
         next();
     }
     catch (err){
-        console.log(err);
+        console.log("Incoming unauthorized request");
         return res.redirect("/login");
     }
 }
