f52571f876971144c195c145024d6dda4592900e
Phanxv <phankawee2003@gmail.com>
2024-04-17 13:01:17 +0700

    create users list page

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../middleware/upload");
-const { jwtAuth } = require("../middleware/jwtAuth")
-const { createPost, fetchData, wipeDatabase, serveFile } = require("../controllers/postController");
+const { jwtAuth } = require("../middleware/jwtAuth");
+const User = require("../models/User")
 
-router.post("/createPost", upload.single('attachment'), createPost);
+const {
+  createPost,
+  fetchData,
+  wipeDatabase,
+  serveFile,
+} = require("../controllers/postController");
+
+router.post("/createPost", upload.single("attachment"), createPost);
 router.post("/wipeDatabase", wipeDatabase);
 router.get("/fetch/:data", fetchData);
 router.get("/storage/:fileId", jwtAuth, serveFile);
+
+router.get("/users", jwtAuth, async (req, res) => {
+  const users = await User.find().lean().exec();
+  res.render("users", { users });
+});
 
 module.exports = router;
